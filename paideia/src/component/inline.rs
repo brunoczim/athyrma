828411a03784a4b07ecc5643e5aaaae9439d94f5
brunoczim <brunoczim@gmail.com>
2022-09-23 23:42:27 -0300

    corrected a few errors

--- paideia/src/component/inline.rs
+++ paideia/src/component/inline.rs
@@ -1,4 +1,12 @@
-use super::ComponentKind;
+use super::{
+    Component,
+    ComponentKind,
+    Context,
+    HtmlRendering,
+    MdRendering,
+    Render,
+};
+use std::fmt;
 
 pub mod text;
 
@@ -8,3 +16,105 @@ pub struct InlineComponent {
 }
 
 impl ComponentKind for InlineComponent {}
+
+fn html_escape(ch: char) -> Option<&'static str> {
+    match ch {
+        '&' => Some("&amp;"),
+        '<' => Some("&lt;"),
+        '>' => Some("&gt;"),
+        '"' => Some("&quot;"),
+        '\'' => Some("&#39;"),
+        '\\' => Some("&#92;"),
+        _ => None,
+    }
+}
+
+fn md_escape(ch: char) -> Option<&'static str> {
+    match ch {
+        '*' => Some("\\*"),
+        '-' => Some("\\-"),
+        '`' => Some("\\`"),
+        '_' => Some("\\_"),
+        '(' => Some("\\("),
+        ')' => Some("\\)"),
+        '[' => Some("\\["),
+        ']' => Some("\\]"),
+        _ => html_escape(ch),
+    }
+}
+
+impl Component for str {
+    type Kind = InlineComponent;
+}
+
+impl Render<HtmlRendering> for str {
+    fn render(
+        &self,
+        fmtr: &mut fmt::Formatter,
+        _ctx: &Context<HtmlRendering, Self::Kind>,
+    ) -> fmt::Result {
+        let mut start = 0;
+        let iter = self
+            .char_indices()
+            .filter_map(|(i, ch)| html_escape(ch).map(|s| (i, s)));
+
+        for (end, escape) in iter {
+            fmtr.write_str(&self[start .. end])?;
+            fmtr.write_str(escape)?;
+            start = end + 1;
+        }
+
+        fmtr.write_str(&self[start ..])?;
+        Ok(())
+    }
+}
+
+impl Render<MdRendering> for str {
+    fn render(
+        &self,
+        fmtr: &mut fmt::Formatter,
+        _ctx: &Context<MdRendering, Self::Kind>,
+    ) -> fmt::Result {
+        let mut start = 0;
+        let iter = self
+            .char_indices()
+            .filter_map(|(i, ch)| md_escape(ch).map(|s| (i, s)));
+
+        for (end, escape) in iter {
+            fmtr.write_str(&self[start .. end])?;
+            fmtr.write_str(escape)?;
+            start = end + 1;
+        }
+
+        fmtr.write_str(&self[start ..])?;
+        Ok(())
+    }
+}
+
+impl_text_as_display! { str }
+
+impl Component for String {
+    type Kind = InlineComponent;
+}
+
+impl Render<HtmlRendering> for String {
+    fn render(
+        &self,
+        fmtr: &mut fmt::Formatter,
+        ctx: &Context<HtmlRendering, Self::Kind>,
+    ) -> fmt::Result {
+        (**self).render(fmtr, ctx)
+    }
+}
+
+impl Render<MdRendering> for String {
+    fn render(
+        &self,
+        fmtr: &mut fmt::Formatter,
+        ctx: &Context<MdRendering, Self::Kind>,
+    ) -> fmt::Result {
+        (**self).render(fmtr, ctx)
+    }
+}
+
+impl_text_as_display! { String }
